ba5ad86a349a313459202c4a61a5dd05ccfe246a
Marjoriee03 <142650841+Marjoriee03@users.noreply.github.com>
2023-10-15 18:40:12 +0800

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,20 @@
+package Cenosin.Array;
+
+public class ArraySize {
+
+  public static void main(String[] args) {
+   int[] ArraySize = {11,22,33,44,55,66,77,88,99,100};
+   
+   System.out.println(ArraySize [0]);
+   System.out.println(ArraySize [1]);
+   System.out.println(ArraySize [2]);
+   System.out.println(ArraySize [3]);
+   System.out.println(ArraySize [4]);
+   System.out.println(ArraySize [5]);
+   System.out.println(ArraySize [6]);
+   System.out.println(ArraySize [7]);
+   System.out.println(ArraySize [8]);
+   System.out.println(ArraySize [9]);
+   
+  }
+     }
